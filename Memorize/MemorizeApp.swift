3c0e0daff349b94bb63c1ec60e01aa634e339bf9
ReturnZer0 <raofei23@gmail.com>
2020-10-17 23:13:53 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 饶飞 on 2020/10/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
