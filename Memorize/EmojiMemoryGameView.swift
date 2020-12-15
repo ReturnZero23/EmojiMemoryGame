c94722fb68d34c1537d80176ab2158c85c3c1a2a
ReturnZer0 <raofei23@gmail.com>
2020-12-16 00:26:39 +0800

    chore: remove debug

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
         VStack {
             Grid(viewModel.getCards()) { card in
                 CardView(card: card).onTapGesture(){
-                    withAnimation(Animation.linear(duration: 0.75).repeatCount(3)) {
+                    withAnimation(Animation.linear(duration: 0.75)) {
                         self.viewModel.choose(card: card)
                     }
                 }
